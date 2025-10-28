e27500dd7805a309a6d07bfc0316563fcd6e3726
rivero117 <134765956+rivero117@users.noreply.github.com>
2025-10-28 11:26:44 +0900

    ver 1.0.0

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/rivero117/homebrew-dote"
   url "https://github.com/rivero117/homebrew-dote/main/dote.c"
   sha256 "81947352327c6a943b72a8180b129c7169fc4e395b70b272aa032f1c8a70fbe7"
+  version "1.0.0"
 
   def install
     system ENW.cc, "dote.c","-o","dote"
