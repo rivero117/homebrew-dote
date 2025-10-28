34c3b00bcdd359a4c3aeff5f887c46fa3553f273
rivero117 <134765956+rivero117@users.noreply.github.com>
2025-10-28 12:06:34 +0900

    ver 1.0.2

--- dote.rb
+++ dote.rb
@@ -3,7 +3,7 @@ class Dote < Formula
   homepage "https://github.com/rivero117/homebrew-dote"
   url "https://raw.githubusercontent.com/rivero117/homebrew-dote/main/dot9.c"
   sha256 "81947352327c6a943b72a8180b129c7169fc4e395b70b272aa032f1c8a70fbe7"
-  version "1.0.1"
+  version "1.0.2"
 
   def install
     system ENV.cc, "dot9.c", "-o", "dote"
