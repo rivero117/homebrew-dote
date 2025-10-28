7002eeb56a555ded9f43b7172c9297af2b9b19e0
rivero117 <134765956+rivero117@users.noreply.github.com>
2025-10-28 11:46:26 +0900

    なんかちがったから直した

--- dote.rb
+++ dote.rb
@@ -1,7 +1,7 @@
 class Dote < Formula
   desc "dote e command"
   homepage "https://github.com/rivero117/homebrew-dote"
-  url "https://github.com/rivero117/homebrew-dote/main/dote.c"
+  url "https://raw.githubusercontent.com/rivero117/homebrew-dote/main/dot9.c"
   sha256 "81947352327c6a943b72a8180b129c7169fc4e395b70b272aa032f1c8a70fbe7"
   version "1.0.0"
 
