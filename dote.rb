fc5ba7299cf1b8e512fc8ca545d6fbc0a38296da
rivero117 <134765956+rivero117@users.noreply.github.com>
2025-10-28 14:04:24 +0900

    dote9になってた

--- dote.rb
+++ dote.rb
@@ -2,11 +2,11 @@ class Dote < Formula
   desc "dote e command"
   homepage "https://github.com/rivero117/homebrew-dote"
   url "https://raw.githubusercontent.com/rivero117/homebrew-dote/main/dot9.c"
-  sha256 "81947352327c6a943b72a8180b129c7169fc4e395b70b272aa032f1c8a70fbe7"
+  sha256 "e6eb866d86b2f638446febe2af161d2c8c826b79db5c82a86dd8963b5b703f3d"
   version "1.0.2"
 
   def install
-    system ENV.cc, "dot9.c", "-o", "dote"
+    system ENV.cc, "dote.c", "-o", "dote"
     bin.install "dote"
   end
 end
